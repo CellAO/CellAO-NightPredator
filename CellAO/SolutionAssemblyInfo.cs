995782ed038828db500f9967c57a91a3ed2d98d9
Algorithman <algorithman.chris@googlemail.com>
2013-10-26 19:31:10 +0200

    Added SolutionAssemblyInfo to Database and Utility projects

--- CellAO/SolutionAssemblyInfo.cs
+++ CellAO/SolutionAssemblyInfo.cs
@@ -39,6 +39,6 @@ using Utility;
 
 #endregion
 
-[assembly: AssemblyVersion("1.4.1.1")]
-[assembly: AssemblyFileVersion("1.4.1.1")]
+[assembly: AssemblyVersion("1.4.1.3")]
+[assembly: AssemblyFileVersion("1.4.1.3")]
 [assembly: RevisionName("Night Predator")]
