5748df711b9f349d4dfe1b4292a96f3e7810db8e
Algorithman <algorithman.chris@googlemail.com>
2013-10-26 21:02:50 +0200

    complete resharper run

--- CellAO/Libraries/Source/Database/Misc.cs
+++ CellAO/Libraries/Source/Database/Misc.cs
@@ -1,7 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region License
+
+// Copyright (c) 2005-2013, CellAO Team
+// 
+// 
+// All rights reserved.
+// 
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// 
+// 
+//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//     * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+// 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// Last modified: 2013-10-26 21:02
+// Created:       2013-10-26 20:50
+
+#endregion
 
 namespace CellAO.Database
 {
@@ -181,7 +209,9 @@ namespace CellAO.Database
                                         {
                                             if (queries[counter].Trim() != string.Empty)
                                             {
-                                                part = queries[counter].Substring(queries[counter].ToLower().IndexOf("values"));
+                                                part =
+                                                    queries[counter].Substring(
+                                                        queries[counter].ToLower().IndexOf("values"));
                                                 part = part.Substring(part.IndexOf("(")); // from '(' to end
                                                 part = part.Substring(0, part.Length - 1); // Remove ';'
                                                 if (Buffer.Length + 1 + part.Length > 1024 * 1000)
@@ -192,7 +222,8 @@ namespace CellAO.Database
                                                     Buffer.Clear();
                                                     Buffer.Append(buf1);
                                                     string lp2 =
-                                                        Convert.ToInt32(Math.Floor((double)counter / queries.Length * 100))
+                                                        Convert.ToInt32(
+                                                            Math.Floor((double)counter / queries.Length * 100))
                                                                .ToString();
                                                     if (lp2 != lastpercent)
                                                     {
@@ -214,7 +245,8 @@ namespace CellAO.Database
                                         conn.Execute(Buffer.ToString());
                                         Buffer.Clear();
                                         string lp =
-                                            Convert.ToInt32(Math.Floor((double)counter / queries.Length * 100)).ToString();
+                                            Convert.ToInt32(Math.Floor((double)counter / queries.Length * 100))
+                                                   .ToString();
                                         if (lp != lastpercent)
                                         {
                                             Console.Write("\rTable " + fName.PadRight(67) + "[" + lp.PadLeft(3) + "%]");
