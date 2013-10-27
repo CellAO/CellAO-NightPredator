8ed6e100c95e5e5533caa13e06288ae69e4c270c
Algorithman <algorithman.chris@googlemail.com>
2013-10-27 09:08:26 +0100

    Moved linked SolutionAssemblyInfos -> Properties folder, added NLog nuget

--- CellAO/Libraries/Source/CellAO.Core/Properties/AssemblyInfo.cs
+++ CellAO/Libraries/Source/CellAO.Core/Properties/AssemblyInfo.cs
@@ -47,7 +47,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CellAO.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -60,17 +59,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("5f7951d6-7f73-4a08-a7df-586f413d91fa")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- CellAO/SolutionAssemblyInfo.cs
+++ CellAO/SolutionAssemblyInfo.cs
@@ -39,7 +39,7 @@ using Utility;
 
 #endregion
 
-[assembly: AssemblyVersion("1.4.1.53")]
-[assembly: AssemblyFileVersion("1.4.1.53")]
+[assembly: AssemblyVersion("1.4.1.55")]
+[assembly: AssemblyFileVersion("1.4.1.55")]
 [assembly: RevisionName("Night Predator")]
 [assembly: AssemblyCopyright("Copyright © 2013 CellAO Team")]
