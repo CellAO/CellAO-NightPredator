c338a2f68ecde8cb2d8ca360c6e2294cc8dd70b9
Algorithman <algorithman.chris@googlemail.com>
2013-11-01 10:58:24 +0100

    ArmorInventoryPage

--- CellAO/SolutionAssemblyInfo.cs
+++ CellAO/SolutionAssemblyInfo.cs
@@ -39,7 +39,7 @@ using Utility;
 
 #endregion
 
-[assembly: AssemblyVersion("1.4.1.304")]
-[assembly: AssemblyFileVersion("1.4.1.304")]
+[assembly: AssemblyVersion("1.4.1.305")]
+[assembly: AssemblyFileVersion("1.4.1.305")]
 [assembly: RevisionName("Night Predator")]
 [assembly: AssemblyCopyright("Copyright © 2013 CellAO Team")]
