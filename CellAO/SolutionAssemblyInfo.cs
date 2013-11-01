7eece465dc9e0b6014f5c485506a22d6f7649080
Algorithman <algorithman.chris@googlemail.com>
2013-11-01 07:51:07 +0100

    changed nlog in Cell.Core to NLog 2.1.0.0

--- CellAO/SolutionAssemblyInfo.cs
+++ CellAO/SolutionAssemblyInfo.cs
@@ -39,7 +39,7 @@ using Utility;
 
 #endregion
 
-[assembly: AssemblyVersion("1.4.1.294")]
-[assembly: AssemblyFileVersion("1.4.1.294")]
+[assembly: AssemblyVersion("1.4.1.296")]
+[assembly: AssemblyFileVersion("1.4.1.296")]
 [assembly: RevisionName("Night Predator")]
 [assembly: AssemblyCopyright("Copyright © 2013 CellAO Team")]
