89e345d6e8c3bf652618295d17a4b27438a3a1f8
Algorithman <algorithman.chris@googlemail.com>
2013-11-01 11:09:46 +0100

    PlayerShopInventoryPage

--- CellAO/Libraries/Source/CellAO.Core/Inventory/PlayerShopInventoryPage.cs
+++ CellAO/Libraries/Source/CellAO.Core/Inventory/PlayerShopInventoryPage.cs
@@ -26,20 +26,30 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // 
-// Last modified: 2013-10-30 22:52
-// Created:       2013-10-30 17:25
+// Last modified: 2013-11-01 11:09
+// Created:       2013-11-01 11:08
 
 #endregion
 
-#region Usings ...
+namespace CellAO.Core.Inventory
+{
+    #region Usings ...
 
-using System.Reflection;
+    using SmokeLounge.AOtomation.Messaging.GameData;
 
-using Utility;
+    #endregion
 
-#endregion
-
-[assembly: AssemblyVersion("1.4.1.308")]
-[assembly: AssemblyFileVersion("1.4.1.308")]
-[assembly: RevisionName("Night Predator")]
-[assembly: AssemblyCopyright("Copyright © 2013 CellAO Team")]
+    /// <summary>
+    /// </summary>
+    public class PlayerShopInventoryPage : BaseInventoryPage
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="ownerInstance">
+        /// </param>
+        public PlayerShopInventoryPage(int ownerInstance)
+            : base((int)IdentityType.PlayerShopInventory, 0x40, 0, ownerInstance)
+        {
+        }
+    }
+}
