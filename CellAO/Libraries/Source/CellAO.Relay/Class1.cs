330f844b35ead926b4ecaa2a3c7ca406ccd47878
Darkkitten <darkkitten@gmail.com>
2013-11-04 14:53:53 -0700

    Added a Source for The Relay Bot to IRC
    Base for Relay Bot

---
+++ CellAO/Libraries/Source/CellAO.Relay/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellAO.Relay
+{
+    public class Class1
+    {
+    }
+}
