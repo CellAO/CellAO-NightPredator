4b9273df3e6c07bc02de9d1c34b0f1c5de5385f4
Darkkitten <darkkitten@gmail.com>
2013-11-04 15:05:16 -0700

    Getting the CellAO.Relay restructured
    Added the common class to the CellAO.Relay Bot

---
+++ CellAO/Libraries/Source/CellAO.Relay/Common/ConsoleUtilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellAO.Relay.Common
+{
+    public static class ConsoleUtilities
+    {
+        public static void WriteError(string message, params string[] args)
+        {
+            UseTextColour(ConsoleColor.Red, () => Console.Error.WriteLine(message, args));
+        }
+
+        public static void UseTextColour(ConsoleColor colour, Action action)
+        {
+            var prevForegroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = colour;
+            action();
+            Console.ForegroundColor = prevForegroundColor;
+        }
+    }
+}
---
+++ CellAO/Libraries/Source/CellAO.Relay/Common/IrcBotException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellAO.Relay.Common
+{
+    public class IrcBotException : Exception
+    {
+        public IrcBotException(IrcBotExceptionType type, string message)
+            : base(message)
+        {
+        }
+    }
+
+    public enum IrcBotExceptionType
+    {
+        Unknown,
+        NoConnection,
+    }
+}
