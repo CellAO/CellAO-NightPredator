776dd380ebde39c1135a114319c9b1452e553300
Algorithman <algorithman.chris@googlemail.com>
2013-11-10 17:56:42 +0100

    Added SolutionAssemblyInfo to CellAO.Relay

--- CellAO/Libraries/Source/CellAO.Relay/Properties/AssemblyInfo.cs
+++ CellAO/Libraries/Source/CellAO.Relay/Properties/AssemblyInfo.cs
@@ -10,7 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CellAO.Relay")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("94f7a7b6-c22c-4475-8ba4-21ac72f78fd5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
