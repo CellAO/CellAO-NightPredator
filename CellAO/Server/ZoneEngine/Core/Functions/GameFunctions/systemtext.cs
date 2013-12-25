978172dc174ed9f5545bfc3ffbb4fd1c28586aad
Algorithman <algorithman.chris@googlemail.com>
2013-12-25 16:21:47 +0100

    added SystemText (FormatFeedback) function, not perfect, handles only one string argument yet

---
+++ CellAO/Server/ZoneEngine/Core/Functions/GameFunctions/systemtext.cs
@@ -0,0 +1,99 @@
+﻿#region License
+
+// Copyright (c) 2005-2013, CellAO Team
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//     * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+
+namespace ZoneEngine.Core.Functions.GameFunctions
+{
+    #region Usings ...
+
+    using CellAO.Core.Entities;
+    using CellAO.Enums;
+
+    using MsgPack;
+
+    using SmokeLounge.AOtomation.Messaging.Messages.N3Messages;
+
+    #endregion
+
+    /// <summary>
+    /// </summary>
+    internal class systemtext : FunctionPrototype
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// </summary>
+        public override FunctionType FunctionId
+        {
+            get
+            {
+                return FunctionType.SystemText;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// </summary>
+        /// <param name="self">
+        /// </param>
+        /// <param name="caller">
+        /// </param>
+        /// <param name="target">
+        /// </param>
+        /// <param name="arguments">
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public override bool Execute(
+            INamedEntity self,
+            INamedEntity caller,
+            IInstancedEntity target,
+            MessagePackObject[] arguments)
+        {
+            string text = arguments[0].AsString();
+            byte b = 0;
+            var message = new FormatFeedbackMessage()
+            {
+                Identity = self.Identity,
+                Message = text + (char)b,
+                Unknown1 = 0,
+                DataLength = (short)(text.Length + 0xf),
+                Unknown2 = 0x7e26,
+                Unknown3 = 0x21212122,
+                Unknown4 = 0x3a212121,
+                Unknown5 = 0x293C,
+                Unknown6 = 0x73,
+                Unknown7 = 0
+            };
+            ((ICharacter)self).Send(message);
+            return true;
+        }
+
+        #endregion
+    }
+}
