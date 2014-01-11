fa3f0970b56d92e09e5ea890e6c48392b7117a13
Algorithman <algorithman.chris@googlemail.com>
2014-01-11 11:38:12 +0100

    bit less clutter - removed Destinations and Walls xml files

--- CellAO/SolutionAssemblyInfo.cs
+++ CellAO/SolutionAssemblyInfo.cs
@@ -86,7 +86,7 @@ using Utility;
 
 #endregion
 
-[assembly: AssemblyVersion("1.4.1.1344")]
-[assembly: AssemblyFileVersion("1.4.1.1344")]
+[assembly: AssemblyVersion("1.4.1.1345")]
+[assembly: AssemblyFileVersion("1.4.1.1345")]
 [assembly: RevisionName("Night Predator")]
 [assembly: AssemblyCopyright("Copyright © 2014 CellAO Team")]
