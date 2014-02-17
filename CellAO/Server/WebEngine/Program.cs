4b5e893944767b3364ade0741e5e4278e04764f2
Darkkitten <darkkitten@gmail.com>
2014-02-17 14:12:28 -0700

    Update?
    Update

--- CellAO/Server/WebEngine/Program.cs
+++ CellAO/Server/WebEngine/Program.cs
@@ -1,24 +1,61 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
-using System.Web;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-using System.IO;
-using System.Xml;
-using System.Text;
+﻿    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    using NBug;
+    using NBug.Properties;
+
+    using Utility;
+
+    using Config = Utility.Config.ConfigReadWrite;
 
 namespace WebEngine
 {
-    class Program
+    using System.ComponentModel;
+
+    internal class Program
     {
-        static void Main(string[] args)
+        #region Static Fields
+        public static bool DebugNetwork;
+
+
+        private static ServerConsoleCommands consoleCommands = new ServerConsoleCommands();
+
+        private static bool exited = false;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// </summary>
+        public static void StartTheServer()
         {
             var myServer = default(HttpServer);
             myServer = new HttpServer();
+        }
+
+        #endregion
+
+        private static bool InitializeConsoleCommands()
+        {
+            consoleCommands.Engine = "Login";
+
+            consoleCommands.AddEntry("start", StartServer);
+            consoleCommands.AddEntry("running", IsServerRunning);
+            consoleCommands.AddEntry("stop", StopServer);
+            consoleCommands.AddEntry("exit", ShutDownServer);
+            consoleCommands.AddEntry("quit", ShutDownServer);
+            consoleCommands.AddEntry("debugnetwork", SetDebugNetwork);
+            consoleCommands.AddEntry("checkphp", CheckPHP);
+            consoleCommands.AddEntry("checkWebCore", CheckWebCore);
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+
         }
     }
 }
