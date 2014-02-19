bd5be6a8c9f6440c8fe6e2d6fb9e3614557af0f7
Darkkitten <darkkitten@gmail.com>
2014-02-19 11:23:42 -0700

    Added download for php zip file, now  need to add response when it's downloading to unzip it the directory choosen in config and delete the zip

--- CellAO/Server/WebEngine/Checks.cs
+++ CellAO/Server/WebEngine/Checks.cs
@@ -8,6 +8,7 @@ using _config = Utility.Config.ConfigReadWrite;
 namespace WebEngine
 {
     using System.IO;
+    using System.Net;
 
     public static class Checks
     {
@@ -15,7 +16,25 @@ namespace WebEngine
         {
             if (Directory.Exists(_config.Instance.CurrentConfig.WebHostPhpPath) == false)
             {
+                var request = (HttpWebRequest)WebRequest.Create("http://windows.php.net/downloads/releases/php-5.5.9-nts-Win32-VC11-x86.zip");
+                request.Method = "GET";
+                request.ContentType = "application/zip";
+                try
+                {
 
+                    var res = (HttpWebResponse)request.GetResponse();
+                    using (var sr = new StreamReader(res.GetResponseStream(), Encoding.Default))
+                    {
+                        var oWriter = new StreamWriter(@"php-5.5.9-nts-Win32-VC11-x86.zip");
+                        oWriter.Write(sr.ReadToEnd());
+                        oWriter.Close();
+                    }
+                    res.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex);
+                }
             }
             else { Console.WriteLine("Php exists.");}
         }
