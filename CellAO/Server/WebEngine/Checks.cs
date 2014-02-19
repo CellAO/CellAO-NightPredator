4a44ea7d02025edc8f34f2294b742080c301fd43
Darkkitten <darkkitten@gmail.com>
2014-02-19 10:56:11 -0700

    Fixed a spelling mistake Added Base for Checks.cs

--- CellAO/Server/WebEngine/Checks.cs
+++ CellAO/Server/WebEngine/Checks.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _config = Utility.Config.ConfigReadWrite;
 
 namespace WebEngine
 {
+    using System.IO;
+
     public static class Checks
     {
+        public static void CheckPhp()
+        {
+            if (Directory.Exists(_config.Instance.CurrentConfig.WebHostPhpPath) == false)
+            {
+
+            }
+            else { Console.WriteLine("Php exists.");}
+        }
+
+        public static void CheckWebCore()
+        {
+            if (Directory.Exists(_config.Instance.CurrentConfig.WebHostRoot) == false)
+            {
+                
+            }
+            else { Console.WriteLine("Webcore Exists.");}
+        }
     }
 }
