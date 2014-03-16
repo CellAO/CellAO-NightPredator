2aff3f472ccf84fd09714a8f7a50cef873e95288
Algorithman <algorithman.chris@googlemail.com>
2014-03-16 13:34:09 +0100

    KnuBot, but still missing helper functions

--- CellAO/SolutionAssemblyInfo.cs
+++ CellAO/SolutionAssemblyInfo.cs
@@ -86,7 +86,7 @@ using Utility;
 
 #endregion
 
-[assembly: AssemblyVersion("1.4.1.1635")]
-[assembly: AssemblyFileVersion("1.4.1.1740")]
+[assembly: AssemblyVersion("1.4.1.1641")]
+[assembly: AssemblyFileVersion("1.4.1.1746")]
 [assembly: RevisionName("Night Predator")]
 [assembly: AssemblyCopyright("Copyright © 2014 CellAO Team")]
