87d1851599fe933c523a1844b545866b2585844f
Darkkitten <darkkitten@gmail.com>
2014-03-24 23:51:13 -0600

    Added the base for a CellAO Launcher in Tools/Ashly/CellAO-Launcher

---
+++ Tools/Ashly/CellAO-Launcher/CellAO-Launcher/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CellAO_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bx_IPAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UseEncryption = new System.Windows.Forms.CheckBox();
+            this.bx_AOExe = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Launch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server IP Address:";
+            // 
+            // bx_IPAddress
+            // 
+            this.bx_IPAddress.Location = new System.Drawing.Point(27, 90);
+            this.bx_IPAddress.Name = "bx_IPAddress";
+            this.bx_IPAddress.Size = new System.Drawing.Size(157, 20);
+            this.bx_IPAddress.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(453, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 43);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CellAO Launcher";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "AO Executable:";
+            // 
+            // UseEncryption
+            // 
+            this.UseEncryption.AutoSize = true;
+            this.UseEncryption.Location = new System.Drawing.Point(819, 184);
+            this.UseEncryption.Name = "UseEncryption";
+            this.UseEncryption.Size = new System.Drawing.Size(144, 17);
+            this.UseEncryption.TabIndex = 5;
+            this.UseEncryption.Text = "Use Encrypted Launcher";
+            this.UseEncryption.UseVisualStyleBackColor = true;
+            // 
+            // bx_AOExe
+            // 
+            this.bx_AOExe.Location = new System.Drawing.Point(27, 133);
+            this.bx_AOExe.Name = "bx_AOExe";
+            this.bx_AOExe.Size = new System.Drawing.Size(437, 20);
+            this.bx_AOExe.TabIndex = 6;
+            this.bx_AOExe.TextChanged += new System.EventHandler(this.bx_AOExe_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(486, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Find AO exe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 214);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bx_AOExe);
+            this.Controls.Add(this.UseEncryption);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bx_IPAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox bx_IPAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox UseEncryption;
+        private System.Windows.Forms.TextBox bx_AOExe;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Tools/Ashly/CellAO-Launcher/CellAO-Launcher/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CellAO_Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog browseFile = new OpenFileDialog();
+            browseFile.Filter = "Exe Files (*.exe)|*.exe";
+            browseFile.Title = "Browse EXE files";
+
+            if (browseFile.ShowDialog() == DialogResult.Cancel)
+                return;
+            try
+            {
+                bx_AOExe.Text = browseFile.FileName;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex);
+            }
+        }
+
+        private void bx_AOExe_TextChanged(object sender, EventArgs e)
+        {
+        
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] temp = bx_IPAddress.Text.Split('.');
+            int ipConverted = int.Parse(temp[3]) + int.Parse(temp[2]) * 256 + int.Parse(temp[1]) * 256 * 256 + int.Parse(temp[0]) * 256 * 256 * 256;
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            if (UseEncryption.Checked == true)
+            {
+                MessageBox.Show("Not Implamented yet.");
+            }
+            startInfo.FileName = bx_AOExe.Text;
+            startInfo.Arguments = Convert.ToString(ipConverted);
+            Process.Start(startInfo);
+        }
+    }
+}
