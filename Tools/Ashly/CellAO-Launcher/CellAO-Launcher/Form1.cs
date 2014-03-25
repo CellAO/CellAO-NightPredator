d8d968e86af2a139b16b9d6c5e3d7c0a55829958
Darkkitten <darkkitten@gmail.com>
2014-03-25 14:45:54 -0600

    Added Keys for the start of Use Encryption Removed unused Usings..

--- Tools/Ashly/CellAO-Launcher/CellAO-Launcher/Form1.cs
+++ Tools/Ashly/CellAO-Launcher/CellAO-Launcher/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using _config = CellAO_Launcher.Config.ConfigReadWrite;
@@ -20,6 +13,15 @@ namespace CellAO_Launcher
         {
             InitializeComponent();
         }
+
+        private string FUNCOM_KEY_PUBLIC = "9c32cc23d559ca90fc31be72df817d0e124769e809f936bc14360ff4bed758f260a0d596584eacbbc2b88bdd410416163e11dbf62173393fbc0c6fefb2d855f1a03dec8e9f105bbad91b3437d8eb73fe2f44159597aa4053cf788d2f9d7012fb8d7c4ce3876f7d6cd5d0c31754f4cd96166708641958de54a6def5657b9f2e92";
+        private string FUNCOM_KEY_PRIME =  "eca2e8c85d863dcdc26a429a71a9815ad052f6139669dd659f98ae159d313d13c6bf2838e10a69b6478b64a24bd054ba8248e8fa778703b418408249440b2c1edd28853e240d8a7e49540b76d120d3b1ad2878b1b99490eb4a2a5e84caa8a91cecbdb1aa7c816e8be343246f80c637abc653b893fd91686cf8d32d6cfe5f2a6f";
+        private int FUNCOM_KEY_GENERATOR = 5;
+
+        //Referance Only
+        private string CELLAO_KEY_PRIVATE = "7ad852c6494f664e8df21446285ecd6f400cf20e1d872ee96136d7744887424b";
+        private string CELLAO_KEY_PUBLIC  = "26b5a3b4ac1177f24a2d9de44bafef477ff23ef1cb5f646919b1be26516053030b65d5afb60cef6f49de539958ba0b7922a099319b8016a8673cb27a696ae4b60fdece25ddcdad42e7f0056b87fc35687fe033b242e17e960d79806fd46c4a79cbc64f558660a50cabc1c242dace70de6af452e3433f97e30e202567f187de70";
+        private int CELLAO_KEY_GENERATOR = 5;
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -75,7 +77,6 @@ namespace CellAO_Launcher
                 {
                     startInfo.FileName = bx_AOExe.Text;
                     startInfo.Arguments = " IA " + ipConverted + " IP " + bx_Port.Text + " UI";
-                    //startInfo.WorkingDirectory = Path.GetDirectory(bx_AOExe.Text);
                     startInfo.WorkingDirectory = Path.GetDirectoryName(bx_AOExe.Text);
                     Process.Start(startInfo);
 
