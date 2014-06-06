10220ccc8f7abb7f61f7ac48ee48a1880fd41efb
Algorithman <algorithman.chris@googlemail.com>
2014-06-06 18:10:51 +0200

    config.xml fix

--- CellAO/SolutionAssemblyInfo.cs
+++ CellAO/SolutionAssemblyInfo.cs
@@ -37,7 +37,7 @@ using Utility;
 
 #endregion
 
-[assembly: AssemblyVersion("1.4.1.3022")]
-[assembly: AssemblyFileVersion("1.4.1.3127")]
+[assembly: AssemblyVersion("1.4.1.3024")]
+[assembly: AssemblyFileVersion("1.4.1.3129")]
 [assembly: RevisionName("Night Predator")]
 [assembly: AssemblyCopyright("Copyright © 2014 CellAO Team")]
