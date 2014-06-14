03fb0782ebe0de9ae3e980fbdbf828afdce82bd9
Algorithman <algorithman.chris@googlemail.com>
2014-06-14 17:40:43 +0200

    2nd try

--- CellAO/SolutionAssemblyInfo.cs
+++ CellAO/SolutionAssemblyInfo.cs
@@ -37,7 +37,7 @@ using Utility;
 
 #endregion
 
-[assembly: AssemblyVersion("1.4.2.3349")]
-[assembly: AssemblyFileVersion("1.4.2.3449")]
+[assembly: AssemblyVersion("1.4.2.3350")]
+[assembly: AssemblyFileVersion("1.4.2.3450")]
 [assembly: RevisionName("Night Predator")]
 [assembly: AssemblyCopyright("Copyright © 2014 CellAO Team")]
