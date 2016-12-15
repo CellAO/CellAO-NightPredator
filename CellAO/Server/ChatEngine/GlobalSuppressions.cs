69724ae0c8c0bdeae9e65a5dabb0292d0df132f7
darkkitten <darkkitten@gmail.com>
2016-12-15 00:40:11 -0700

    Added Suppression File

---
+++ CellAO/Server/ChatEngine/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Object initialization can be simplified", Justification = "<Pending>", Scope = "member", Target = "~M:ChatEngine.Relay.Common.IrcBot.Connect(System.String,IrcDotNet.IrcRegistrationInfo)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:ChatEngine.Relay.Common.IrcBot.client_ChannelListReceived(System.Object,IrcDotNet.IrcChannelListReceivedEventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0018:Inline variable declaration", Justification = "<Pending>", Scope = "member", Target = "~M:ChatEngine.Relay.Common.IrcBot.ReadChatCommand(IrcDotNet.IrcClient,IrcDotNet.IIrcMessageSource,System.Collections.Generic.IList{IrcDotNet.IIrcMessageTarget},System.String,System.String[])")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0018:Inline variable declaration", Justification = "<Pending>", Scope = "member", Target = "~M:ChatEngine.Relay.Common.IrcBot.ReadCommand(System.String,System.Collections.Generic.IList{System.String})")]
+
